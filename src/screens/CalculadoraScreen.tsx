5b9feaaf308e9a1bec01c1c2f86cb0dfdf2df656
gejaga12 <gejaga12@hotmail.com>
2022-09-23 13:51:58 -0300

    Primer commit

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {BotonCalc} from '../components/BotonCalc';
+import {styles} from '../theme/appTheme';
+
+export const CalculadoraScreen = () => {
+  return (
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+      <View style={styles.fila}>
+
+        <BotonCalc texto="C" color="#9B9B9B"/>
+        <BotonCalc texto="+/-" color="#9B9B9B"/>
+        <BotonCalc texto="del" color="#9B9B9B"/>
+        <BotonCalc texto="/" color="#FF9427"/>
+        
+        
+
+        {/* gris oscuro #2D2D2D
+        naranja #FF9427 */}
+
+      </View>
+    </View>
+  );
+};
